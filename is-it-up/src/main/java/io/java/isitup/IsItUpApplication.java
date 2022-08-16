71fb48ca1935207e9580e92b0d62d1720bf08a58
Satyaansh <satyanshasthana@gmail.com>
2022-08-16 21:13:44 +0530

    Initial Commit

---
+++ is-it-up/src/main/java/io/java/isitup/IsItUpApplication.java
@@ -0,0 +1,13 @@
+package io.java.isitup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsItUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsItUpApplication.class, args);
+	}
+
+}
---
+++ is-it-up/src/main/java/io/java/isitup/controllers/UrlCheckController.java
@@ -0,0 +1,46 @@
+package io.java.isitup.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UrlCheckController {
+
+    private final String SITE_IS_UP = "Site is UP!";
+    private final String SITE_IS_DOWN = "Site is DOWN!";
+    private final String INCORRECT_URL = "URL is incorrect!";
+    
+    @GetMapping("/check")
+    public String getUrlStatusMessage(@RequestParam String url) {
+
+        String returnMessage = "";
+        
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responseCodeCategory = conn.getResponseCode() / 100;
+            if(responseCodeCategory != 2 && responseCodeCategory != 3) {
+                returnMessage = SITE_IS_DOWN;
+            }
+            else {
+                returnMessage = SITE_IS_UP;
+            }
+        } catch (MalformedURLException e) {
+            returnMessage = INCORRECT_URL;
+        } catch (IOException e) {
+            returnMessage = SITE_IS_DOWN;
+        }
+
+
+        return returnMessage;
+    }
+
+}
